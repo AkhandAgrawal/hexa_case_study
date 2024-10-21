937ae14d6495e40d61954e573abce2f34eb0d444
Akhand Agrawal <83289867+AkhandAgrawal@users.noreply.github.com>
2024-10-22 02:45:36 +0530

    Update test_lease_retrieval.py

--- Testing/test_lease_retrieval.py
+++ Testing/test_lease_retrieval.py
@@ -3,7 +3,7 @@ from dao.icar_lease_repository_impl import ICarLeaseRepositoryImpl
 def test_retrieve_lease():
     service_provider = ICarLeaseRepositoryImpl()
     
-    lease_id = 301  # Example lease ID
+    lease_id = 301 
     retrieved_lease = service_provider.return_car(lease_id)
     
-    assert retrieved_lease is not None  # Check if the lease is retrieved successfully
+    assert retrieved_lease is not None  
