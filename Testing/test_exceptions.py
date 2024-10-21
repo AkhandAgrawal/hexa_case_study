04a3ba06b9f2aa68ff5cd2ca220bbeecd0e9f4f5
Akhand Agrawal <83289867+AkhandAgrawal@users.noreply.github.com>
2024-10-22 02:45:16 +0530

    Update test_exceptions.py

--- Testing/test_exceptions.py
+++ Testing/test_exceptions.py
@@ -6,16 +6,16 @@ def test_customer_not_found_exception():
     service_provider = ICarLeaseRepositoryImpl()
     
     with pytest.raises(CustomerNotFoundException):
-        service_provider.find_customer_by_id(999)  # Example customer ID that doesn't exist
+        service_provider.find_customer_by_id(999) 
 
 def test_car_not_found_exception():
     service_provider = ICarLeaseRepositoryImpl()
     
     with pytest.raises(CarNotFoundException):
-        service_provider.find_car_by_id(999)  # Example car ID that doesn't exist
+        service_provider.find_car_by_id(999) 
 
 def test_lease_not_found_exception():
     service_provider = ICarLeaseRepositoryImpl()
     
     with pytest.raises(LeaseNotFoundException):
-        service_provider.return_car(999)  # Example lease ID that doesn't exist
+        service_provider.return_car(999) 
